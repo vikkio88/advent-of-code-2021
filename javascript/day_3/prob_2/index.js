92e4c0ae418b1463aa9df50737b8927364165ee9
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2021-12-03 19:24:15 +0000

    less iterations, you moron

--- javascript/day_3/prob_2/index.js
+++ javascript/day_3/prob_2/index.js
@@ -1,35 +1,30 @@
 //const input = require('./data/input.json');
 const input = require('./data/example.json');
 
-const calculateBitsCount = input => {
-    // assuming the same amount of bits
-    const numberOfBits = input[0].length;
-    const bits = Array.from({length:numberOfBits}, () => ({0:0,1:0}));
-
+const calculateBitsCount = (input, position) => {
+    const bits = { 0: 0, 1: 0 };
     for (const line of input) {
         const inputBits = [...line];
-        for (const index in inputBits) {
-            const value = inputBits[index];
-            bits[index][value] += 1;
-        }
+
+        const value = inputBits[position];
+        bits[value] += 1;
     }
 
     return bits;
-}
+};
 
 const findValue = (lines, condition) => {
     let value = null;
     let index = 0;
-    while(lines.length > 1) {
+    while (lines.length > 1) {
         // this could only go and count at index pos
-        const bits = calculateBitsCount(lines);
-        const bitCount = bits[index];
-        const [maxAmount,bitToCheck] = condition(bitCount);
-        
+        const bitCount = calculateBitsCount(lines, index);
+        const [maxAmount, bitToCheck] = condition(bitCount);
+
         const remainingLines = [];
         for (const line of lines) {
             if (line[index] === bitToCheck) remainingLines.push(line);
-            if(remainingLines.length >= maxAmount) break;
+            if (remainingLines.length >= maxAmount) break;
         }
 
         lines = remainingLines;
@@ -38,20 +33,20 @@ const findValue = (lines, condition) => {
     }
 
     return value.join('');
-}
+};
 
 const oxy = findValue(input, bitCount => {
-    if (bitCount[1] >= bitCount[0]) return [bitCount[1],'1'];
+    if (bitCount[1] >= bitCount[0]) return [bitCount[1], '1'];
     return [bitCount[0], '0'];
 });
 
 
 const co2 = findValue(input, bitCount => {
-    if (bitCount[0] <= bitCount[1]) return [bitCount[0],'0'];
+    if (bitCount[0] <= bitCount[1]) return [bitCount[0], '0'];
     return [bitCount[1], '1'];
 });
-let oxyDEC = parseInt(oxy ,2);
-let co2DEC = parseInt(co2 ,2);
+let oxyDEC = parseInt(oxy, 2);
+let co2DEC = parseInt(co2, 2);
 console.log(`oxy: ${oxy}, co2: ${co2}\nDEC: o2: ${oxyDEC} , co2: ${co2DEC}\n\t-> solution: ${oxyDEC * co2DEC}`);
 
 /**
